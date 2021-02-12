15461f792505d3e0ef1640753cbba1b0ada20bc1
Mikael B <mikaelbihouise@hotmail.fr>
2021-02-12 13:30:02 +0100

    updated user model

--- models/users.js
+++ models/users.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose')
 
+const wishlistSchema = mongoose.Schema({
+    title: String,
+    content: String,
+    description: String,
+    urltoimage: String,
+})
+
 const userSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
     token: String,
+    wishlist: [wishlistSchema],
 })
 
 const userModel = mongoose.model('users', userSchema)
